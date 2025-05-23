458e0d68442cd92968fba78d3b75de7a2655dccf
satish <satishkonduru0225@gmail.com>
2025-05-23 19:12:29 +0530

    Style Binding

--- src/app/components/style-binding/style-binding.component.ts
+++ src/app/components/style-binding/style-binding.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-style-binding',
   templateUrl: './style-binding.component.html',
-  styleUrls: ['./style-binding.component.css']
+  styleUrls: ['./style-binding.component.css'],
 })
 export class StyleBindingComponent {
-
+  textColor = 'tomato';
+  textSize = '30px';
+  myValue = 10;
+  myStyles = {
+    color: this.textColor,
+    fontSize: this.textSize,
+  };
 }
