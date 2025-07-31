4d2e7f202541b39ed96961c927f15825f19c20b8
satish <satishkonduru0225@gmail.com>
2025-07-31 08:05:34 +0530

    ContentChild

--- src/app/components/modal-body/modal-body.component.ts
+++ src/app/components/modal-body/modal-body.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-modal-body',
   templateUrl: './modal-body.component.html',
-  styleUrls: ['./modal-body.component.css']
+  styleUrls: ['./modal-body.component.css'],
 })
 export class ModalBodyComponent {
+  bodyMessage = 'Hello from Modal Body!';
 
+  getBodyMessage(): string {
+    return this.bodyMessage;
+  }
 }
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,9 +1,33 @@
 import { Component, ContentChild } from '@angular/core';
 import { ModalFooterComponent } from '../modal-footer/modal-footer.component';
+import { ModalHeaderComponent } from '../modal-header/modal-header.component';
+import { ModalBodyComponent } from '../modal-body/modal-body.component';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {}
+export class ModalComponent {
+  @ContentChild(ModalHeaderComponent)
+  headerComponent?: ModalHeaderComponent;
+  @ContentChild(ModalBodyComponent) bodyComponent?: ModalBodyComponent;
+  @ContentChild(ModalFooterComponent)
+  footerComponent?: ModalFooterComponent;
+
+  headerAvailable: boolean = false;
+  bodyAvailable: boolean = false;
+  footerAvailable: boolean = false;
+  bodyMessage: string = '';
+  ngAfterContentInit(): void {
+    this.headerAvailable = !!this.headerComponent;
+    this.bodyAvailable = !!this.bodyComponent;
+    this.footerAvailable = !!this.footerComponent;
+  }
+
+  getBodyMessage() {
+    if (this.bodyComponent) {
+      this.bodyMessage = this.bodyComponent.getBodyMessage();
+    }
+  }
+}
