3be1f51c1c8c3873fa8a5e5b30582301e2a9b114
satish <satishkonduru0225@gmail.com>
2025-08-05 08:17:07 +0530

    @HostBinding

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,21 +1,40 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective {
+  // private highlightColor: string = 'yellow';
+  // @Input() set appHighlight(color: string | '') {
+  //   this.highlightColor = color?.trim() || 'yellow';
+  // }
+  // constructor(private el: ElementRef) {}
+  // @HostListener('mouseenter') onMouseEnter() {
+  //   this.el.nativeElement.style.backgroundColor = this.highlightColor;
+  // }
+  // @HostListener('mouseleave') onMouseLeave() {
+  //   this.el.nativeElement.style.backgroundColor = '';
+  // }
+
   private highlightColor: string = 'yellow';
   @Input() set appHighlight(color: string | '') {
     this.highlightColor = color?.trim() || 'yellow';
   }
 
-  constructor(private el: ElementRef) {}
+  @HostBinding('style.backgroundColor') backgroundColor?: string;
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.el.nativeElement.style.backgroundColor = this.highlightColor;
+    console.log('Highlight color:', this.highlightColor);
+   this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.el.nativeElement.style.backgroundColor = '';
+    this.backgroundColor = '';
   }
 }
