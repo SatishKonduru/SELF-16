c7458f1b6485c5940bbaa129d8dd7d4bad2e8cfd
satish <satishkonduru0225@gmail.com>
2025-09-07 18:59:39 +0530

    Routes

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
-  styleUrls: ['./course-details.component.css']
+  styleUrls: ['./course-details.component.css'],
 })
 export class CourseDetailsComponent {
-
+  courseList = [
+    { id: 1, name: 'Angular', tutor: 'Satish' },
+    { id: 2, name: 'React', tutor: 'RSK' },
+    { id: 3, name: 'Angular Material', tutor: 'Satish' },
+    { id: 4, name: 'Bootstrap', tutor: 'Satish Konduru' },
+    { id: 5, name: 'NodeJS', tutor: 'Renu' },
+  ];
+  courseKeys: string[] = [];
+  ngOnInit() {
+    this.courseKeys = Object.keys(this.courseList[0]);
+  }
 }
--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
 })
 export class CourseComponent {
-
+  courseList = [
+    { id: 1, name: 'Angular', tutor: 'Satish' },
+    { id: 2, name: 'React', tutor: 'RSK' },
+    { id: 3, name: 'Angular Material', tutor: 'Satish' },
+    { id: 4, name: 'Bootstrap', tutor: 'Satish Konduru' },
+    { id: 5, name: 'NodeJS', tutor: 'Renu' },
+  ];
 }
