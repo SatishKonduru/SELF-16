0815d97be5a1e1d65543ea3737fd1ebd3e500372
satish <satishkonduru0225@gmail.com>
2025-10-30 10:43:37 +0530

    working on Impure Changes

--- src/app/components/cusotm-pipe/cusotm-pipe.component.ts
+++ src/app/components/cusotm-pipe/cusotm-pipe.component.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class CusotmPipeComponent {
   name = 'Satish';
-  score = 777;
+  // score = 777;
+  score: any;
   total = 1000;
+  filterText = 'all';
+  students = [
+    { name: 'Satish', age: 45, gender: 'Male' },
+    { name: 'Renu', age: 37, gender: 'Female' },
+    { name: 'RSK', age: 44, gender: 'Male' },
+    { name: 'Micky', age: 20, gender: 'Female' },
+  ];
+  onClick(marks: any) {
+    this.score = marks;
+  }
 }
--- src/app/pipes/percentage.pipe.ts
+++ src/app/pipes/percentage.pipe.ts
@@ -1,12 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'percentage'
+  name: 'percentage',
 })
 export class PercentagePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(score: any, total: any, decimal = 2): unknown {
+    console.log('Percentage Pipe Called...');
+    return ((score / total) * 100).toFixed(decimal) + '%';
   }
-
 }
