e163694d6365dd7ee1a45c7abcf64d1b3d06b8db
satish <satishkonduru0225@gmail.com>
2026-01-19 07:58:40 +0530

    validations

--- src/app/forms/tdf-register/tdf-register.component.ts
+++ src/app/forms/tdf-register/tdf-register.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tdf-register',
   templateUrl: './tdf-register.component.html',
-  styleUrls: ['./tdf-register.component.css']
+  styleUrls: ['./tdf-register.component.css'],
 })
 export class TdfRegisterComponent {
-
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
 }
